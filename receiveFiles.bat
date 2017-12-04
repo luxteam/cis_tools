82959a72aeac780533f83636e26c912c562c8f04
radeonprorender <radeonprorender.buildmaster@gmail.com>
2017-12-04 11:40:02 +0100

    fix bash path

--- receiveFiles.bat
+++ receiveFiles.bat
@@ -5,6 +5,6 @@ set REMOTE_SOURCE=%1
 set LOCAL_DIR=%2
 set REMOTE_PATH=%REMOTE_ROOT%/%REMOTE_SOURCE%
 
-bash.exe -c 'mkdir -p %LOCAL_DIR%'
+c:\\JN\\bash.exe -c 'mkdir -p %LOCAL_DIR%'
 
 c:\\JN\\bash.exe -c 'scp -r %REMOTE_HOST%:%REMOTE_PATH% %LOCAL_DIR%'
--- sendFiles.bat
+++ sendFiles.bat
@@ -4,6 +4,6 @@ set REMOTE_ROOT=/var/data/storage.rpr/www/html
 set REMOTE_DIR=%2
 set REMOTE_PATH=%REMOTE_ROOT%/%REMOTE_DIR%
 
-bash.exe -c 'ssh %REMOTE_HOST% "mkdir -p %REMOTE_PATH%"'
+c:\\JN\\bash.exe -c 'ssh %REMOTE_HOST% "mkdir -p %REMOTE_PATH%"'
 
 c:\\JN\\bash.exe -c 'scp -r %SOURCE% %REMOTE_HOST%:%REMOTE_PATH%'
