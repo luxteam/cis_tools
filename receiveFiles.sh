e7be4cf38e24f529e7c2ca34d99d389a81dacfb6
user <user@kaa-osx.local>
2018-01-19 17:56:47 +0300

    unix files

---
+++ receiveFiles.sh
@@ -0,0 +1,11 @@
+REMOTE_HOST=admin@172.30.23.112
+REMOTE_ROOT=/var/data/storage.rpr/www/html
+
+REMOTE_SOURCE=$1
+LOCAL_DIR=$2
+REMOTE_PATH=${REMOTE_ROOT}/${REMOTE_SOURCE}
+
+mkdir -p ${LOCAL_DIR}
+
+scp -r ${REMOTE_HOST}:${REMOTE_PATH} ${LOCAL_DIR}
+
