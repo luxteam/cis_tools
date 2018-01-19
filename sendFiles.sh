39cc6bbe4850bb1d3c6e25a3122e60850d40e9f0
user <user@kaa-osx.local>
2018-01-19 17:56:47 +0300

    unix files

---
+++ sendFiles.sh
@@ -0,0 +1,9 @@
+SOURCE=$1
+REMOTE_HOST=admin@172.30.23.112
+REMOTE_ROOT=/var/data/storage.rpr/www/html
+REMOTE_DIR=$2
+REMOTE_PATH=${REMOTE_ROOT}/${REMOTE_DIR}
+
+ssh ${REMOTE_HOST} "mkdir -p ${REMOTE_PATH}"
+
+scp -r ${SOURCE} ${REMOTE_HOST}:${REMOTE_PATH}
