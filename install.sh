8a9b6a4563809f956808de078e8b669a6509dc7b
admin <admin@LC-CENTOS7-BUILDER01.luxoft.com>
2018-02-03 18:44:49 +0200

    execu scripts

--- install.sh
+++ install.sh
@@ -5,25 +5,16 @@ SCRIPTPATH=`dirname $SCRIPT`
 
 CIS_TOOLS=${SCRIPTPATH}
 JENKINS_ROOT=${SCRIPTPATH}/..
-JN_RUN_SCRIPT=${CIS_TOOLS}/../runJenkinsAgent.sh
+JN_RUN_OPTIONS=${SCRIPTPATH}/runJenkinsAgent.conf
 
 #ssh-keygen -t rsa
 #ssh-copy-id admin@172.30.23.112
 
-[ ! -f ${CIS_TOOLS}/../ ] && echo '# jenkins agent execution script' >> $JN_RUN_SCRIPT
-chmod +x $JN_RUN_SCRIPT
+[ ! -f ${JN_RUN_OPTIONS} ] && echo '# jenkins agent options'  >> $JN_RUN_OPTIONS
 
+HOSTNAME=`hostname`
 
-JENKINS_SECRET=145678901890190123457890123457890
-JENKINS_SERVER=https://rpr.cis.luxoft.com
-JENKINS_AGENTNAME=`hostname`
+echo 'JENKINS_SECRET=145678901890190123457890123457890' >> $JN_RUN_OPTIONS
+echo 'JENKINS_SERVER=https://rpr.cis.luxoft.com'        >> $JN_RUN_OPTIONS
+echo 'JENKINS_AGENTNAME=`hostname`'                     >> $JN_RUN_OPTIONS
 
-wget --directory-prefix={JENKINS_ROOT} ${JENKINS_SERVER}/jnlpJars/agent.jar
-
-
-while true
-do
-    echo "connecting..."
-    java -jar agent.jar -jnlpUrl ${JENKINS_SERVER}/computer/${JENKINS_AGENTNAME}/slave-agent.jnlp -secret ${JENKINS_SECRET} -workDir "${JENKINS_ROOT}"
-    sleep 5
-done
