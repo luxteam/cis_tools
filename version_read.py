ae412ce76e0a2542dca630fed4f659c6397a1b91
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-07 22:56:16 +0300

    Update version_read.py

--- version_read.py
+++ version_read.py
@@ -8,11 +8,13 @@ def main():
 
 	parser.add_argument('--file', required = True, help = 'File with build version string')
 	parser.add_argument('--prefix', required = True, help = 'Prefix before build version')
+	parser.add_argument('--delimiter', default = ".", required = True, help = 'Delimiter between numbers in version')
 
 	args = parser.parse_args()
 
 	file = args.file
 	prefix = args.prefix
+	delimiter = args.delimiter
 
 	old_version = []
 
@@ -23,14 +25,14 @@ def main():
 
 	try:
 
-		old_version = re.findall(r'\d+.\d+.\d+.\d+', prefix_line)
+		old_version = re.findall('\d'+ delimiter +'\d'+ delimiter+'\d'+ delimiter + '\d+', prefix_line)
 
 		if len(old_version) == 0:
-			old_version = re.findall(r'\d+.\d+.\d+', prefix_line)
+			old_version = re.findall('\d'+ delimiter +'\d'+ delimiter+'\d', prefix_line)
 			if len(old_version) == 0:
-				old_version = re.findall(r'\d+.\d+', prefix_line)
+				old_version = re.findall('\d'+ delimiter +'\d', prefix_line)
 				if len(old_version) == 0: 
-					old_version = re.findall(r'\d+', prefix_line)
+					old_version = re.findall('\d+', prefix_line)
 
 		if len(old_version) == 0:
 			print("Unsupported version. No numbers in prefix line.")
