2970a8cec0e85925aab97f48c2308da34593ed99
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-02-08 23:19:48 +0300

    Update version_inc.py

--- version_inc.py
+++ version_inc.py
@@ -9,18 +9,18 @@ def main():
     parser.add_argument('--index', type=int, default = 1, help = 'Index of inc number (default = 1)')
     parser.add_argument('--version', required = True, help = 'Version of build')
     parser.add_argument('--inc', type=int, default = 1, help = 'Increment number (default = 1)')
-    parser.add_argument('--delimiter', default = ".", help = 'Delimiter between numbers in version')
+    parser.add_argument('--delimeter', default = ".", help = 'Delimeter between numbers in version')
 
     args = parser.parse_args()
 
     index = args.index
     version = args.version
     inc = args.inc
-    delimiter = args.delimiter
+    delimeter = args.delimeter
 
-    version_split = version.split(delimiter)
+    version_split = version.split(delimeter)
     version_split[index - 1] = str(int(version_split[index-1])+ inc)
-    print(delimiter.join(version_split))
+    print(delimeter.join(version_split))
 
 if __name__ == "__main__":
 
