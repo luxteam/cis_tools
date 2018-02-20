7be287c9c1a1872a2958bcf13fb06a4416dbbd96
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-02-20 17:48:17 +0300

    Update receiveFiles.bat

--- receiveFiles.bat
+++ receiveFiles.bat
@@ -7,4 +7,5 @@ set REMOTE_PATH=%REMOTE_ROOT%/%REMOTE_SOURCE%
 
 c:\\JN\\bash.exe -c 'mkdir -p %LOCAL_DIR%'
 
-c:\\JN\\bash.exe -c 'scp -r %REMOTE_HOST%:%REMOTE_PATH% %LOCAL_DIR%'
+rem c:\\JN\\bash.exe -c 'scp -r %REMOTE_HOST%:%REMOTE_PATH% %LOCAL_DIR%'
+c:\\JN\\bash.exe -c 'rsync -rvz %REMOTE_HOST%:%REMOTE_PATH% %LOCAL_DIR%'
