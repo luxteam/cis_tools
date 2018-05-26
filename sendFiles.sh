4316668eb51bbdbf94a56f9577214cd1f96daf4a
Alekseevich Andrey <MagisterDemens@users.noreply.github.com>
2018-05-26 16:02:34 +0300

    Update sendFiles.sh

--- sendFiles.sh
+++ sendFiles.sh
@@ -1,6 +1,6 @@
 SOURCE=$1
 REMOTE_HOST=admin@172.30.23.112
-REMOTE_ROOT=/var/data/storage.rpr/www/html
+REMOTE_ROOT=/home/admin/Server/storage.rpr/www/html
 REMOTE_DIR=$2
 REMOTE_PATH=${REMOTE_ROOT}/${REMOTE_DIR}
 
