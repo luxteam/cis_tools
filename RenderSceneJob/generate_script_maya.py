c22eec5953f7a5ceaae6cb1949bea023f082a10b
Andrey Kravchenko <epozine@inbox.ru>
2018-06-16 14:51:35 +0300

    Update generate_script_maya.py

--- RenderSceneJob/generate_script_maya.py
+++ RenderSceneJob/generate_script_maya.py
@@ -6,14 +6,15 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--folder')
 	parser.add_argument('--scene')
+	parser.add_argument('--pass_limit')
+	parser.add_argument('--render_device')
 	args = parser.parse_args()
 	
 	folder = args.folder
-	scene = args.scene
 
 	with open(os.path.join(folder, "maya_render.mel")) as f:
             mel_template = f.read()
-	melScript = mel_template.format(scene=scene)
+	melScript = mel_template.format(scene=args.scene, pass_limit=args.pass_limit, render_device=args.render_device)
 
 	with open(os.path.join(folder, "maya_render.mel"), 'w') as f:
             f.write(melScript)
