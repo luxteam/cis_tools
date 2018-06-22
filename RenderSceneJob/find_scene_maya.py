4195e40c18cf359556fd368e6264fc5ff503aba9
Kravchenko Andrey <epozine@inbox.ru>
2018-06-22 04:05:23 +0300

    fix license problem

--- RenderSceneJob/find_scene_maya.py
+++ RenderSceneJob/find_scene_maya.py
@@ -1,6 +1,16 @@
 import os
 import argparse
 
+def update_license(file):
+	with open(file) as f:
+		scene_file = f.read()
+
+	license = "fileInfo \"license\" \"student\";"
+	scene_file = scene_file.replace(license, '')
+
+	with open(file, "w") as f:
+		f.write(scene_file)
+		
 
 def main():
 
@@ -14,11 +24,14 @@ def main():
 	for rootdir, dirs, files in os.walk(folder):
 		for file in files:
 			if file.split('.')[-1] == 'ma' or file.split('.')[-1] == 'mb':
+				update_license(file)
 				scene.append(file)
 	if " " in scene[0]:
 		os.rename(scene[0], scene[0].replace(" ", "_"))
 		scene[0] = scene[0].replace(" ", "_")
 	print (scene[0])
+
+
 	
 
 if __name__ == "__main__":
