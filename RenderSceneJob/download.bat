a72e1d1919f0307964c1b11e8eec42484596bdfc
Andrey Kravchenko <epozine@inbox.ru>
2018-07-16 12:29:52 +0300

    update scripts for render service

--- RenderSceneJob/download.bat
+++ RenderSceneJob/download.bat
@@ -1,3 +1,3 @@
 set REMOTE_SOURCE=%1
 
-bash -c 'wget -O scene.zip %REMOTE_SOURCE%'
+bash -c 'wget %REMOTE_SOURCE%'
--- RenderSceneJob/download.sh
+++ RenderSceneJob/download.sh
@@ -1,3 +1,3 @@
 REMOTE_SOURCE=$1
 
-wget -O scene.zip ${REMOTE_SOURCE}
+wget ${REMOTE_SOURCE}
