3a5ac97111275ae9c09ef7f29173cc9b54ec7534
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2018-11-21 13:31:44 +0300

    Create receiveFilesSync.bat

---
+++ receiveFilesSync.bat
@@ -0,0 +1,9 @@
+set REMOTE_HOST=admin@172.30.23.112
+set REMOTE_ROOT=/home/admin/Server/storage.rpr/www/html
+
+set REMOTE_SOURCE=%1
+set LOCAL_DIR=%2
+set REMOTE_PATH=%REMOTE_ROOT%/%REMOTE_SOURCE%
+
+bash -c 'mkdir -p %LOCAL_DIR%'
+bash -c 'rsync -rvzc --delete %REMOTE_HOST%:%REMOTE_PATH% %LOCAL_DIR%'
