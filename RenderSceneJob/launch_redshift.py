c27b5106dfde19824e385d667fc225c2d7dc571b
Andrey Kravchenko <epozine@inbox.ru>
2018-12-18 13:16:02 +0300

    update conversion scripts

--- RenderSceneJob/launch_redshift.py
+++ RenderSceneJob/launch_redshift.py
@@ -2,12 +2,31 @@ import argparse
 import os
 import subprocess
 import psutil
-import json
 import ctypes
 import pyscreenshot
-from shutil import copyfile
-import PIL.ImageGrab as IG
 from subprocess import Popen
+
+
+def get_windows_titles():
+	EnumWindows = ctypes.windll.user32.EnumWindows
+	EnumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_int))
+	GetWindowText = ctypes.windll.user32.GetWindowTextW
+	GetWindowTextLength = ctypes.windll.user32.GetWindowTextLengthW
+	IsWindowVisible = ctypes.windll.user32.IsWindowVisible
+
+	titles = []
+
+	def foreach_window(hwnd, lParam):
+		if IsWindowVisible(hwnd):
+			length = GetWindowTextLength(hwnd)
+			buff = ctypes.create_unicode_buffer(length + 1)
+			GetWindowText(hwnd, buff, length + 1)
+			titles.append(buff.value)
+		return True
+
+	EnumWindows(EnumWindowsProc(foreach_window), 0)
+
+	return titles
 
 
 def main():
@@ -20,26 +39,70 @@ def main():
 	parser.add_argument('--sceneName', required=True)
 
 	args = parser.parse_args()
-	
-	with open("redshift_cmd_render.py") as f:
-		py_template = f.read()
 
-	args.sceneName = os.path.basename(args.sceneName)
-
-	pyScript = py_template.format(scene = args.scene, pass_limit = args.pass_limit, scene_name = args.sceneName)
-
-	with open('redshift_cmd_render.py', 'w') as f:
-		f.write(pyScript)
+	current_path = os.getcwd().replace("\\", "/")
+	redshift_scene = os.path.join(current_path, args.scene).replace("\\", "/")
 
 	if not os.path.exists('Output'):
 		os.makedirs('Output')
-		
-	mayapy = "C:\\Program Files\\Autodesk\\Maya{tool}\\bin\\mayapy.exe".format(tool=args.tool)
-	params = [mayapy]
-	params += ["redshift_cmd_render.py"]
+	output_path = os.path.join(current_path, "Output").replace("\\", "/")
+	
+	args.sceneName = os.path.basename(args.sceneName)
 
-	p = Popen(params)
+	# Redshift batch render
+
+	renderer_folder = "C:\\Program Files\\Autodesk\\Maya{tool}\\bin".format(args.tool)
+	renderer_exec_name = "Render"
+	params = [renderer_exec_name]
+	params += ['-r', 'redshift']
+	params += ['-cam', 'camera1']
+	params += ['-im', args.sceneName]
+	params += ['-of', 'jpg']
+	params += ['-rd', output_path]
+	params += [redshift_scene]
+	p = Popen(params, cwd=renderer_folder)
 	stdout, stderr = p.communicate()
+
+
+	with open("maya_convert_render.py") as f:
+		py_template = f.read()
+	
+	pyScript = py_template.format(scene = args.scene, pass_limit = args.pass_limit, scene_name = args.sceneName, res_path=output_path)
+
+	with open('maya_convert_render.py', 'w') as f:
+		f.write(pyScript)
+
+	cmdRun = '''
+	set MAYA_CMD_FILE_OUTPUT=%cd%/renderTool.log 
+	set MAYA_SCRIPT_PATH=%cd%;%MAYA_SCRIPT_PATH%
+	"{tool}" -command "python(import maya_convert_render.py); python(main());"''' \
+		.format(tool=args.tool)
+
+	with open(os.path.join(args.output, 'script.bat'), 'w') as f:
+		f.write(cmdRun)
+
+	os.chdir(args.output)
+	p = psutil.Popen(os.path.join(args.output, 'script.bat'), stdout=subprocess.PIPE)
+	rc = -1
+
+	while True:
+		try:
+			rc = p.wait(timeout=5)
+		except psutil.TimeoutExpired as err:
+			fatal_errors_titles = ['maya', 'Student Version File', 'Radeon ProRender Error', 'Script Editor']
+			if set(fatal_errors_titles).intersection(get_windows_titles()):
+				rc = -1
+				try:
+					error_screen = pyscreenshot.grab()
+					error_screen.save(os.path.join(args.output, 'error_screenshot.jpg'))
+				except:
+					pass
+				for child in reversed(p.children(recursive=True)):
+					child.terminate()
+				p.terminate()
+				break
+		else:
+			break
 
 
 if __name__ == "__main__":
--- RenderSceneJob/maya_convert_render.py
+++ RenderSceneJob/maya_convert_render.py
@@ -18,30 +18,14 @@ def rpr_render():
 	cmds.fireRender(waitForItTwo=True)
 	
 	mel.eval("renderIntoNewWindow render")
-	output = os.path.join("{res_path}", "Output", "{scene_name}")
+	output = os.path.join("{res_path}", "{scene_name}")
 	cmds.renderWindowEditor("renderView", edit=True, dst="color")
 	cmds.renderWindowEditor("renderView", edit=True, com=True, writeImage=output)
 
-
-def redshift_render():
-	
-	if not cmds.pluginInfo("RadeonProRender", q=True, loaded=True):
-		cmds.loadPlugin("RadeonProRender")
-	
-	cmds.setAttr("defaultRenderGlobals.currentRenderer", "redshift", type="string")
-	cmds.setAttr("redshiftOptions.imageFormat", 4)
-	cmds.setAttr("redshiftOptions.jpegQuality", 100)
-	
-	mel.eval("renderIntoNewWindow render")
-	output = os.path.join("{res_path}", "Output", "{scene_name}")
-	cmds.renderWindowEditor("renderView", edit=True, dst="color")
-	cmds.renderWindowEditor("renderView", edit=True, com=True, writeImage=output)
-	
 
 def main():
 
 	cmds.file("{scene}", f=True, options="v=0;", ignoreVersion=True, o=True)
-	redshift_render()
 	convert()
 	rpr_render()
 	evalDeferred("quit -abort");
