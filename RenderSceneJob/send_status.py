a31641b0d7ab5636fbda5dd1179682e6bd9112f3
Andrey Kravchenko <epozine@inbox.ru>
2019-01-04 21:17:21 +0300

    add new script for render service

---
+++ RenderSceneJob/send_status.py
@@ -0,0 +1,25 @@
+import requests
+import config
+import json
+import argparse
+
+
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--build_number')
+	parser.add_argument('--status')
+	parser.add_argument('--id')
+	parser.add_argument('--django_ip')
+	args = parser.parse_args()
+
+	django_url = args.django_ip
+
+	post_data = {'status': args.status, 'Build_number': args.build_number, 'id': args.id}
+	response = requests.post(django_url, data=post_data)
+	print(response)
+
+if __name__ == "__main__":
+
+	main()
+
