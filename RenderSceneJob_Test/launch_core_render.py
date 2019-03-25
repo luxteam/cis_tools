fae7bb09e2a41493f1e666abd16d751eec576b14
Andrey Kravchenko <epozine@inbox.ru>
2019-03-25 12:49:47 +0300

    core animation

--- RenderSceneJob_Test/launch_core_render.py
+++ RenderSceneJob_Test/launch_core_render.py
@@ -16,54 +16,69 @@ def main():
     parser.add_argument('--width', required=True)
     parser.add_argument('--height', required=True)
     parser.add_argument('--sceneName', required=True)
+    parser.add_argument('--startFrame', required=True)
+    parser.add_argument('--endFrame', required=True)
 
     args = parser.parse_args()
+
+    startFrame = int(args.startFrame)
+    endFrame = int(args.endFrame)
 
     current_path = os.getcwd()
     if not os.path.exists('Output'):
         os.makedirs('Output')
     output_path = os.path.join(current_path, "Output")
 
+    timeout = 3600 / (endFrame - startFrame + 1)
 
-    config_json = {}
-    config_json["width"] = int(args.width)
-    config_json["height"] = int(args.height)
-    config_json["gamma"] = 1
-    config_json["iterations"] = int(args.pass_limit)
-    config_json["threads"] = 4
-    config_json["output"] = os.path.join(output_path, args.sceneName + ".png")
-    config_json["output.json"] = os.path.join(output_path, args.sceneName + "_original.json")
-    config_json["context"] = {
-        "gpu0": 1,
-        "gpu1": 0,
-        "threads": 16,
-        "debug": 0
-    }
+    # parse file
+    file_name = args.sceneName.split("_")[0]
+    file_format = args.sceneName.split(".")[1]
 
-    
-    ScriptPath = os.path.join(current_path, "cfg_{}.json".format(args.sceneName))
-    cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool="C:\\rprSdkWin64\\RprsRender64.exe", scene=args.scene, template=ScriptPath)
-    cmdScriptPath = os.path.join(current_path, '{}.bat'.format(args.sceneName))
+    for frame in range(startFrame, endFrame + 1):
 
-    try:
-        with open(ScriptPath, 'w') as f:
-            json.dump(config_json, f, indent=4)
-        with open(cmdScriptPath, 'w') as f:
-            f.write(cmdRun)
-    except OSError as err:
-        pass
+        config_json = {}
+        config_json["width"] = int(args.width)
+        config_json["height"] = int(args.height)
+        config_json["gamma"] = 1
+        config_json["iterations"] = int(args.pass_limit)
+        config_json["threads"] = 4
+        config_json["output"] = os.path.join(output_path, file_name + "_" + str(frame) + ".png")
+        config_json["output.json"] = os.path.join(output_path, file_name + "_" + str(frame) + "_original.json")
+        config_json["context"] = {
+            "gpu0": 1,
+            "gpu1": 0,
+            "threads": 16,
+            "debug": 0
+        }
 
-    p = psutil.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = p.communicate()
-    rc = 0
+        # change render scene
+        scene_name = args.scene.split("\\")[-1].split(".")[0]
+        scene = args.scene.replace(scene_name, file_name + "_" + str(frame))
 
-    try:
-        rc = p.wait(timeout=3600)
-    except psutil.TimeoutExpired as err:
-        rc = -1
-        for child in reversed(p.children(recursive=True)):
-            child.terminate()
-        p.terminate()
+        ScriptPath = os.path.join(current_path, "cfg_{}.json".format(file_name + "_" + str(frame)))
+        cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool="C:\\rprSdkWin64\\RprsRender64.exe", scene=scene, template=ScriptPath)
+        cmdScriptPath = os.path.join(current_path, '{}.bat'.format(file_name + "_" + str(frame)))
+        
+        try:
+            with open(ScriptPath, 'w') as f:
+                json.dump(config_json, f, indent=4)
+            with open(cmdScriptPath, 'w') as f:
+                f.write(cmdRun)
+        except OSError as err:
+            pass
+
+        p = psutil.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        stdout, stderr = p.communicate()
+        rc = 0
+
+        try:
+            rc = p.wait(timeout=timeout)
+        except psutil.TimeoutExpired as err:
+            rc = -1
+            for child in reversed(p.children(recursive=True)):
+                child.terminate()
+            p.terminate()
 
 
 if __name__ == "__main__":
