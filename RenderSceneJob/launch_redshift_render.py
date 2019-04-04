e249af50f223fbf5dd1206247e8f83693c726d0e
Andrey Kravchenko <epozine@inbox.ru>
2019-04-04 15:59:17 +0300

    Update launch_redshift_render.py

--- RenderSceneJob/launch_redshift_render.py
+++ RenderSceneJob/launch_redshift_render.py
@@ -67,7 +67,7 @@ def main():
 	stdout, stderr = p.communicate()
 
 	os.rename("redshift_tool.log", os.path.join("Output", "redshift_tool.log"))	
-	render_time = get_rs_render_time(os.path.join(current_path, "Output", "redshift_tool.log"))
+	render_time = round(get_rs_render_time(os.path.join(current_path, "Output", "redshift_tool.log")), 2)
 	post_data = {'tool': 'Redshift', 'render_time': render_time, 'id': args.id, 'status':'render_info'}
 	response = requests.post(args.django_ip, data=post_data)
 
