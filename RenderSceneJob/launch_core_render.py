d0f7565f6be4e6ca5edf2653c161825cb68a36a7
Andrey Kravchenko <epozine@inbox.ru>
2019-04-05 16:31:13 +0300

    update core render

--- RenderSceneJob/launch_core_render.py
+++ RenderSceneJob/launch_core_render.py
@@ -6,6 +6,15 @@ import psutil
 import json
 import requests
 
+def getScenes():
+	scenes = []
+	for rootdir, dirs, files in os.walk(folder):
+		for file in files:
+			if file.endswith('.rpr'):
+				scenes.append(os.path.join(rootdir, file))
+
+	return scenes
+
 def main():
 
 	parser = argparse.ArgumentParser()
@@ -14,7 +23,6 @@ def main():
 	parser.add_argument('--id', required=True)
 
 	parser.add_argument('--tool', required=True)
-	parser.add_argument('--scene', required=True)
 	parser.add_argument('--pass_limit', required=True)
 	parser.add_argument('--width', required=True)
 	parser.add_argument('--height', required=True)
@@ -32,7 +40,9 @@ def main():
 		os.makedirs('Output')
 	output_path = os.path.join(current_path, "Output")
 
-	timeout = 3600 / (endFrame - startFrame + 1)
+	scenes = getScenes(current_path)
+
+	timeout = 3600 / len(scenes)
 	render_time = 0
 
 	if startFrame == 1 and endFrame == 1:
@@ -40,31 +50,19 @@ def main():
 	else:
 		animation = True
 
-	# parse file
-	sceneName = os.path.basename(args.sceneName)
-	if animation:
-		file_name = '_'.join(sceneName.split("_")[0:-1])
-	else:
+	# single rpr file
+	if len(scenes) == 1:
+		sceneName = os.path.basename(args.sceneName)
 		file_name = sceneName.split(".")[0]
-	file_format = sceneName.split(".")[1]
-
-	for frame in range(startFrame, endFrame + 1):
-
-		if animation:
-			post_data = {'tool': 'Core', 'current_frame': frame, 'id': args.id, 'status':'frame'}
-			response = requests.post(args.django_ip, data=post_data)
+		file_format = sceneName.split(".")[1]
 
 		config_json = {}
 		config_json["width"] = int(args.width)
 		config_json["height"] = int(args.height)
 		config_json["iterations"] = int(args.pass_limit)
 		config_json["threads"] = 4
-		if animation:
-			config_json["output"] = os.path.join(output_path, file_name + "_" + str(frame).zfill(3) + ".png")
-			config_json["output.json"] = os.path.join(output_path, file_name + "_" + str(frame).zfill(3) + "_original.json")
-		else:
-			config_json["output"] = os.path.join(output_path, file_name + ".png")
-			config_json["output.json"] = os.path.join(output_path, file_name + "_original.json")
+		config_json["output"] = os.path.join(output_path, file_name + ".png")
+		config_json["output.json"] = os.path.join(output_path, file_name + "_original.json")
 		config_json["context"] = {
 			"gpu0": 1,
 			"gpu1": 0,
@@ -72,16 +70,9 @@ def main():
 			"debug": 0
 		}
 
-		# change render scene
-		if animation:
-			scene_name = args.scene.split("\\")[-1].split(".")[0]
-			scene = args.scene.replace(scene_name, file_name + "_" + str(frame))	
-		else:
-			scene = args.scene
-
-		ScriptPath = os.path.join(current_path, "cfg_{}.json".format(file_name + "_" + str(frame)))
-		cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool="C:\\rprSdkWin64\\RprsRender64.exe", scene=scene, template=ScriptPath)
-		cmdScriptPath = os.path.join(current_path, '{}.bat'.format(file_name + "_" + str(frame)))
+		ScriptPath = os.path.join(current_path, "cfg_{}.json".format(file_name))
+		cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool="C:\\rprSdkWin64\\RprsRender64.exe", scene=scenes[0], template=ScriptPath)
+		cmdScriptPath = os.path.join(current_path, '{}.bat'.format(file_name))
 		
 		try:
 			with open(ScriptPath, 'w') as f:
@@ -105,13 +96,117 @@ def main():
 
 
 		# post request
-		if animation:
+		with open(os.path.join(output_path, file_name + "_original.json")) as f:
+			data = json.loads(f.read().replace("\\", "\\\\"))
+		render_time = data['render.time.ms'] / 1000
+
+	else if len(scenes) > 1 and animation:
+
+		sceneName = os.path.basename(args.sceneName)
+		file_name = '_'.join(sceneName.split("_")[0:-1])
+		file_format = sceneName.split(".")[1]
+
+		for frame in range(startFrame, endFrame + 1):
+
+			post_data = {'tool': 'Core', 'current_frame': frame, 'id': args.id, 'status':'frame'}
+			response = requests.post(args.django_ip, data=post_data)
+
+			config_json = {}
+			config_json["width"] = int(args.width)
+			config_json["height"] = int(args.height)
+			config_json["iterations"] = int(args.pass_limit)
+			config_json["threads"] = 4
+			config_json["output"] = os.path.join(output_path, file_name + "_" + str(frame).zfill(3) + ".png")
+			config_json["output.json"] = os.path.join(output_path, file_name + "_" + str(frame).zfill(3) + "_original.json")
+			config_json["context"] = {
+				"gpu0": 1,
+				"gpu1": 0,
+				"threads": 16,
+				"debug": 0
+			}
+
+			scene_name = args.scene.split("\\")[-1].split(".")[0]
+			scene = args.scene.replace(scene_name, file_name + "_" + str(frame))	
+
+			ScriptPath = os.path.join(current_path, "cfg_{}.json".format(file_name + "_" + str(frame)))
+			cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool="C:\\rprSdkWin64\\RprsRender64.exe", scene=scene, template=ScriptPath)
+			cmdScriptPath = os.path.join(current_path, '{}.bat'.format(file_name + "_" + str(frame)))
+			
+			try:
+				with open(ScriptPath, 'w') as f:
+					json.dump(config_json, f, indent=4)
+				with open(cmdScriptPath, 'w') as f:
+					f.write(cmdRun)
+			except OSError as err:
+				pass
+
+			p = psutil.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			stdout, stderr = p.communicate()
+			rc = 0
+
+			try:
+				rc = p.wait(timeout=timeout)
+			except psutil.TimeoutExpired as err:
+				rc = -1
+				for child in reversed(p.children(recursive=True)):
+					child.terminate()
+				p.terminate()
+
 			with open(os.path.join(output_path, file_name + "_" + str(frame).zfill(3) + "_original.json")) as f:
 				data = json.loads(f.read().replace("\\", "\\\\"))
-		else:
+			render_time += data['render.time.ms'] / 1000
+
+	else:
+
+		for scene in scenes:
+			sceneName = os.path.basename(scene)
+			file_name = sceneName.split(".")[0]
+			file_format = sceneName.split(".")[1]
+
+			config_json = {}
+			config_json["width"] = int(args.width)
+			config_json["height"] = int(args.height)
+			config_json["iterations"] = int(args.pass_limit)
+			config_json["threads"] = 4
+			config_json["output"] = os.path.join(output_path, file_name + ".png")
+			config_json["output.json"] = os.path.join(output_path, file_name + "_original.json")
+			config_json["context"] = {
+				"gpu0": 1,
+				"gpu1": 0,
+				"threads": 16,
+				"debug": 0
+			}
+
+			ScriptPath = os.path.join(current_path, "cfg_{}.json".format(file_name))
+			cmdRun = '"{tool}" "{scene}" "{template}"\n'.format(tool="C:\\rprSdkWin64\\RprsRender64.exe", scene=scene, template=ScriptPath)
+			cmdScriptPath = os.path.join(current_path, '{}.bat'.format(file_name))
+			
+			try:
+				with open(ScriptPath, 'w') as f:
+					json.dump(config_json, f, indent=4)
+				with open(cmdScriptPath, 'w') as f:
+					f.write(cmdRun)
+			except OSError as err:
+				pass
+
+			p = psutil.Popen(cmdScriptPath, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			stdout, stderr = p.communicate()
+			rc = 0
+
+			try:
+				rc = p.wait(timeout=timeout)
+			except psutil.TimeoutExpired as err:
+				rc = -1
+				for child in reversed(p.children(recursive=True)):
+					child.terminate()
+				p.terminate()
+
+
+			# post request
 			with open(os.path.join(output_path, file_name + "_original.json")) as f:
 				data = json.loads(f.read().replace("\\", "\\\\"))
-		render_time += data['render.time.ms'] / 1000
+			render_time += data['render.time.ms'] / 1000
+
 
 	render_time = round(render_time, 2)
 	post_data = {'tool': 'Core', 'render_time': render_time, 'id': args.id, 'status':'time'}
