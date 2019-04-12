8ff25542c2ed2744e35a389233ce04aa51bdbf0a
Andrey Kravchenko <epozine@inbox.ru>
2019-04-12 11:17:39 +0300

    update core render

--- RenderSceneJob/launch_core_render.py
+++ RenderSceneJob/launch_core_render.py
@@ -165,8 +165,8 @@ def main():
 
 			frame = re.findall(r'_\d*', file_name)
 			if frame:
-				frame = '_' + frame[-1][1:]
-				file_name = '_'.join(sceneName.split("_")[0:-1]) + str(frame).zfill(3)
+				frame = frame[-1][1:]
+				file_name = '_'.join(sceneName.split("_")[0:-1]) + '_' + frame.zfill(3)
 
 			file_format = sceneName.split(".")[1]
 
