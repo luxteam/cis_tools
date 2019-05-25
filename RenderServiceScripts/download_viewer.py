064165c50cc6b9f193af24c1bd27660111a91eb7
Andrey Kravchenko <epozine@inbox.ru>
2019-05-25 11:02:50 +0300

    new scripts

---
+++ RenderServiceScripts/download_viewer.py
@@ -0,0 +1,21 @@
+import requests
+import config
+
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--version')
+	args = parser.parse_args()
+
+	r = requests.get("https://rpr.cis.luxoft.com/job/RadeonProViewerAuto/job/master/{}/artifact/RprViewer.zip"\
+		.format(args.version), auth=(config.jenkins_username, config.jenkins_password), verify=False)
+
+	with open("RprViewer.zip", 'wb') as f:
+		f.write(r.content)
+		
+	print ("SUCCESS")
+
+	
+if __name__ == "__main__":
+
+	main()
