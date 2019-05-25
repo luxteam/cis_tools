073012dc44238a94c16a8756492c6b46b3bebcbb
Andrey Kravchenko <epozine@inbox.ru>
2019-05-25 19:03:51 +0300

    update post

--- RenderServiceScripts/send_viewer_results.py
+++ RenderServiceScripts/send_viewer_results.py
@@ -16,16 +16,15 @@ def main():
 
 	django_url = args.django_ip
 
-	get_json = requests.get("https://rpr.cis.luxoft.com/job/{jenkins_job}/{build_number}/api/json?pretty=true".format(jenkins_job=args.jenkins_job, build_number=args.build_number), \
+	get_json = requests.get("http://rpr.cis.luxoft.com/job/{jenkins_job}/{build_number}/api/json?pretty=true".format(jenkins_job=args.jenkins_job, build_number=args.build_number), \
 		auth=(config.jenkins_username, config.jenkins_password))
 
 	job_json = json.loads(get_json.text)
 
 	artifacts = {}
 	for job in job_json['artifacts']:
-		artifacts[job['fileName']] = "https://rpr.cis.luxoft.com/job/{jenkins_job}/{build_number}/artifact/Output/{art}"\
+		artifacts[job['fileName']] = "http://172.30.23.112:8088/job/{jenkins_job}/{build_number}/artifact/Output/{art}"\
 																			.format(jenkins_job=args.jenkins_job, build_number=args.build_number, art=job['fileName'])
-
 	post_data = {'status': args.status, 'artifacts':str(artifacts), 'id': args.id, 'build_number': args.build_number}
 	response = requests.post(django_url, data=post_data)
 	print(response)
