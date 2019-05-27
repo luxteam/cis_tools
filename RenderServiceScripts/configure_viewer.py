a464891308f4ba8f8131e581e1b12fd4fa4de207
Andrey Kravchenko <epozine@inbox.ru>
2019-05-27 12:03:02 +0300

    update viewer script

--- RenderServiceScripts/configure_viewer.py
+++ RenderServiceScripts/configure_viewer.py
@@ -27,16 +27,38 @@ def main():
 	# set default
 	if ui_config == None:
 		ui_config = "UIConfig.json"
-		
-	# Preparing config files
-	with open('config.json') as f:
-		config = json.load(f)
+
+	config = []
 	config['scene']['path'] = gltf_file
-	config['uiConfig'] = ui_config
-	config['save_frames'] =  "yes"
-	config['engine'] = args.engine
 	config['screen']['width'] = int(args.width)
 	config['screen']['height'] = int(args.height)
+	config['screen']['full_screen'] = "no"
+	config['engine'] = args.engine
+	config['primary_device'] = 0
+	config['use_mgpu'] = "yes"
+	config['use_denoiser'] = "yes"
+	config['loader'] = "fx-gltf"
+	config['render_interop'] = "yes"
+	config['iterations_per_frame'] = 1
+	config['camera'] = 0
+	config['save_frames'] = "no"
+	config['camera_step'] = 0.3
+	config['camera_roll_step'] = 2.0
+	config['camera_fov_step'] = 0.1
+	config['time_mode'] = "real"
+	config['animation'] = "on"
+	config['camera_fov_step'] = 0.1
+	config['camera_fov_step'] = 0.1
+	config['camera_fov_step'] = 0.1
+	config['uiConfig'] = ui_config
+	config['environment_light']['add'] = "no"
+	config['environment_light']['source'] = "sky.hdr"
+	config['environment_light']['intensity'] = 1.5
+	config['default_light']['add'] = "no"
+	config['default_light']['position'] = [0.0, 20.0, 20.0, 1.0]
+	config['default_light']['intensity'] = 5000.0
+	config['textures']['flip_y'] = "no"
+
 	with open('config.json', 'w') as f:
 		json.dump(config, f, indent=' ')
 		
@@ -44,6 +66,12 @@ def main():
 	zip_name = "RPRViewerPack_{}.zip".format(args.version)	
 	st = psutil.Popen('7z a "{}" ./"{}"/*'.format(zip_name, "."), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	st.communicate()
+
+	config['save_frames'] = "yes"
+	config['iterations_per_frame'] = 100
+	#config['exit_after_frame'] = 1
+	with open('config.json', 'w') as f:
+		json.dump(config, f, indent=' ')
 	
 	p = psutil.Popen("RadeonProViewer.exe", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	try:
