6dda6fb7dd6024cb41ecb96c1e1eaf1a9bb36a61
Andrey Kravchenko <epozine@inbox.ru>
2019-05-29 17:50:03 +0300

    update execute script

--- RenderServiceScripts/send_execute_results.py
+++ RenderServiceScripts/send_execute_results.py
@@ -42,7 +42,10 @@ def main():
 		artifacts[job['fileName']] = "http://172.30.23.112:8088/job/{jenkins_job}/{build_number}/artifact/{art}"\
 			.format(jenkins_job=args.jenkins_job, build_number=args.build_number, art=job['fileName'])
 
-	post_data = {'status': args.status, 'artifacts':str(artifacts), 'id': args.id, 'build_number': args.build_number}
+	zip_link = "http://172.30.23.112:8088/job/{jenkins_job}/{build_number}/artifact/*zip*/archive.zip"\
+																					.format(jenkins_job=args.jenkins_job, build_number=args.build_number)
+
+	post_data = {'status': args.status, 'artifacts':str(artifacts), 'id': args.id, 'build_number': args.build_number, 'zip_link': zip_link}
 
 	try_count = 0
 	while try_count < 3:	
