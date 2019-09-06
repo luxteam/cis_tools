756dc7846f57273d296aa902aacb32e10ae6325a
Andrei Kravchenko <epozine@inbox.ru>
2019-09-06 17:26:41 +0300

    Update render service maya scripts

--- RenderServiceScripts/maya_render.py
+++ RenderServiceScripts/maya_render.py
@@ -10,32 +10,25 @@ def initializeRPR():
 		cmds.loadPlugin("RadeonProRender")
 
 	cmds.setAttr("defaultRenderGlobals.currentRenderer", "FireRender", type="string")
+	cmds.setAttr("RadeonProRenderGlobals.completionCriteriaSeconds", 1)
 	cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", 1)
 	cmds.fireRender(waitForItTwo=True)
 	mel.eval("renderIntoNewWindow render")
+
 
 def rpr_render():
 	
 	cmds.setAttr("defaultRenderGlobals.currentRenderer", "FireRender", type="string")
 	cmds.setAttr("defaultRenderGlobals.imageFormat", 8)
-	iterations = {pass_limit}
-	if iterations:
-		cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", iterations)
-	if cmds.getAttr("RadeonProRenderGlobals.completionCriteriaIterations") > 1000:
-		cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", 1000)
-	
-	render_device_type = "{render_device_type}"
-	if render_device_type == "gpu":
-		cmds.optionVar(rm="RPR_DevicesSelected")
-		cmds.optionVar(iva=("RPR_DevicesSelected", 1))
-	elif render_device_type == "cpu":
-		cmds.optionVar(rm="RPR_DevicesSelected")
-		cmds.optionVar(iva=("RPR_DevicesSelected", 0))
-	else:
-		cmds.optionVar(rm="RPR_DevicesSelected")
-		cmds.optionVar(iva=("RPR_DevicesSelected", 1))
-		cmds.optionVar(iva=("RPR_DevicesSelected", 1))
 
+	cmds.setAttr("RadeonProRenderGlobals.completionCriteriaIterations", {max_samples})
+	cmds.setAttr("RadeonProRenderGlobals.completionCriteriaMinIterations", {min_samples})
+	cmds.setAttr("RadeonProRenderGlobals.adaptiveThreshold", {noise_threshold})
+	cmds.setAttr("RadeonProRenderGlobals.completionCriteriaMinutes" 30)
+
+	cmds.optionVar(rm="RPR_DevicesSelected")
+	cmds.optionVar(iva=("RPR_DevicesSelected", 1))
+	
 	cameras = cmds.ls(type="camera")
 	for cam in cameras:
 	    if cmds.getAttr(cam + ".renderable"):
@@ -71,9 +64,12 @@ def rpr_render():
 	report['render_time'] = round(render_time, 2)
 	report['width'] = cmds.getAttr("defaultResolution.width")
 	report['height'] = cmds.getAttr("defaultResolution.height")
-	report['iterations'] = cmds.getAttr("RadeonProRenderGlobals.completionCriteriaIterations")
+	report['min_samples'] = cmds.getAttr("RadeonProRenderGlobals.completionCriteriaMinIterations")
+	report['max_samples'] = cmds.getAttr("RadeonProRenderGlobals.completionCriteriaIterations")
+	report['noise_threshold'] = cmds.getAttr("RadeonProRenderGlobals.adaptiveThreshold")
 	with open(os.path.join(".", "render_info.json"), 'w') as f:
 		json.dump(report, f, indent=4)
+
 
 def main():
 
