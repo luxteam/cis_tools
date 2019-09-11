fc3a8c42311ca0ffc6b01372d93cd48ff52deff6
Andrei Kravchenko <epozine@inbox.ru>
2019-09-11 18:46:38 +0300

    Update redshift render script for render service

--- RenderServiceScripts/launch_maya_redshift.py
+++ RenderServiceScripts/launch_maya_redshift.py
@@ -61,7 +61,7 @@ def main():
 		project = work_path
 
 	# Redshift batch render
-	cmd_render = '''"C:\\Program Files\\Autodesk\\Maya{tool}\\bin\\Render.exe" -r redshift -proj "{project}" -log redshift_tool.log -im {scene_name} -of jpg -rd {output_path} {redshift_scene}'''\
+	cmd_render = '''"set MAYA_SCRIPT_PATH=%CD%;MAYA_SCRIPT_PATH \nC:\\Program Files\\Autodesk\\Maya{tool}\\bin\\Render.exe" -r redshift -preRender "source maya_mel.mel" -proj "{project}" -log redshift_tool.txt -of jpg -rd {output_path} {redshift_scene}'''\
 					.format(tool=args.tool, scene_name = sceneName, output_path=output_path, redshift_scene=redshift_scene, project=project)
 
 	with open(os.path.join(current_path, 'redshift_script.bat'), 'w') as f:
@@ -70,8 +70,8 @@ def main():
 	p = psutil.Popen(os.path.join(current_path, 'redshift_script.bat'))
 	stdout, stderr = p.communicate()
 
-	os.rename("redshift_tool.log", os.path.join("Output", "redshift_tool.log"))	
-	render_time = round(get_rs_render_time(os.path.join(current_path, "Output", "redshift_tool.log")), 2)
+	os.rename("redshift_tool.txt", os.path.join("Output", "redshift_tool.txt"))	
+	render_time = round(get_rs_render_time(os.path.join(current_path, "Output", "redshift_tool.txt")), 2)
 	post_data = {'tool': 'Maya (Redshift)', 'render_time': render_time, 'id': args.id, 'status':'render_info'}
 	response = requests.post(args.django_ip, data=post_data)
 
