aea9ada353ce23c9208320ac8769947822e973c1
Andrei Kravchenko <epozine@inbox.ru>
2019-11-26 18:23:56 +0300

    Add download script for render service

---
+++ RenderServiceScriptsDebug/download_scene.py
@@ -0,0 +1,37 @@
+import requests
+import os
+import config
+import logging
+import argparse
+
+# logging
+logging.basicConfig(filename="python_log.txt", level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--url')
+	parser.add_argument('--id')
+	args = parser.parse_args()
+
+	url_login = "{}/accounts/login/".format(args.url) 
+	client = requests.session()
+	client.get(url_login)
+	csrftoken = client.cookies['csrftoken']
+
+	login_data = {'username': config.django_username, 'password': config.django_password, 'csrfmiddlewaretoken': csrftoken}
+
+	try:
+		client.post(url_login, data=login_data)
+	except:
+		pass
+
+	downloaded_file = client.post('{}/upload/download/{}'.format(args.url, args.id), data=login_data)
+
+	with open('scene.blend', 'wb') as f:
+		f.write(downloaded_file.content)
+	
+	
+if __name__ == "__main__":
+	main()
