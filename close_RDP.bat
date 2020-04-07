1af30f3ce520966c497b69b991d92411f08e4dec
kataderon <kataderon@gmail.com>
2020-04-07 15:20:18 +0300

    improve close_rdp.bat

--- close_RDP.bat
+++ close_RDP.bat
@@ -1,2 +1,3 @@
-echo off
-tscon 1 /dest:console
+set MY_SESSION_ID=unknown
+for /f "tokens=3-4" %%a in ('query session %username%') do @if "%%b"=="Active" set MY_SESSION_ID=%%a
+tscon %MY_SESSION_ID% /dest:console
