3ae0114e7dfe16ff5ca6c4d55dc84c78518dcb43
Alekseevich Andrey <11044999+MagisterDemens@users.noreply.github.com>
2020-09-09 17:02:58 +0300

    Update sendFiles.bat

--- sendFiles.bat
+++ sendFiles.bat
@@ -4,8 +4,6 @@ set REMOTE_ROOT=/home/admin/Server/storage.rpr/www/html
 set REMOTE_DIR=%2
 set REMOTE_PATH=%REMOTE_ROOT%/%REMOTE_DIR%
 
-bash.exe -c 'ssh %REMOTE_HOST% "mkdir -p %REMOTE_PATH%"'
+bash.exe -c "ssh %REMOTE_HOST% 'mkdir -p %REMOTE_PATH%'"
 
-rem c:\\JN\\bash.exe -c 'scp -r %SOURCE% %REMOTE_HOST%:%REMOTE_PATH%'
-
-bash.exe -c 'rsync -rvzcW %SOURCE% %REMOTE_HOST%:%REMOTE_PATH%'
+bash.exe -c "rsync -rvzcW %SOURCE% %REMOTE_HOST%:%REMOTE_PATH%"
