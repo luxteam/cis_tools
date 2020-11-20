b60f7b52b999ea773cfd0915f213b69a2cd025c6
Ilia Nemankov <INemankov@luxoft.com>
2020-11-20 16:21:40 +0300

    Add from machines scripts for install houdini

---
+++ installHoudiniLinux.sh
@@ -0,0 +1,2 @@
+cd $1
+./houdini.install --auto-install --install-houdini --no-install-menus --no-install-hfs-symlink --install-license --no-install-bin-symlink --make-dir --no-root-check --accept-EULA 2020-05-05 $2
