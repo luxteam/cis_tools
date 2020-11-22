6d404e209ea86f6fe0bc2bcc47c3812900991559
Ilia Nemankov <INemankov@luxoft.com>
2020-11-22 16:31:36 +0300

    Pass REMOTE_HOST as a param of scripts

--- sendFiles.bat
+++ sendFiles.bat
@@ -1,7 +1,7 @@
 set SOURCE=%1
-set REMOTE_HOST=admin@172.30.23.112
 set REMOTE_ROOT=/home/admin/Server/storage.rpr/www/html
 set REMOTE_DIR=%2
+set REMOTE_HOST=%3
 set REMOTE_PATH=%REMOTE_ROOT%/%REMOTE_DIR%
 
 bash.exe -c "ssh %REMOTE_HOST% 'mkdir -p %REMOTE_PATH%'"
