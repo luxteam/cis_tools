96aeecf0c8915aa9b19246c1d3c238bb668a1e1a
Andrei Kravchenko <epozine@inbox.ru>
2021-03-29 15:34:59 +0300

    Add macos support for IPP install

--- ippInstaller.sh
+++ ippInstaller.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+echo $OSTYPE
+
 cd $1
-./bootstrapper --action $2 --silent --log-dir logs --eula accept
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    ./bootstrapper --action $2 --silent --log-dir logs --eula accept
+    
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    hdiutil attach $1 -mountpoint /Volumes/IPP
+    cd /Volumes/IPP/bootstrapper.app/Contents/MacOS
+	./bootstrapper --action $2 --silent --log-dir logs --eula accept
+	cd /home
+    hdiutil detach /Volumes/IPP
+fi
+
+
+
+
+
