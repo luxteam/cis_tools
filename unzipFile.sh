3fef3248e9df911fd93691ce5373d4752ee0204d
Ilia Nemankov <INemankov@luxoft.com>
2021-04-21 13:31:17 +0300

    Small updates for use uploadFiles and unzipFile scripts with stashes

--- unzipFile.sh
+++ unzipFile.sh
@@ -3,9 +3,9 @@
 # $3 - Remote target path
 # $4 - Remove archive
 
-ssh $1 "7z x $2 -o$3"
+ssh $1 "7z x \"$2\" -o\"$3\""
 
 if [ "$4" = "true" ]
 then
-    ssh $1 "rm $2"
+    ssh $1 "rm \"$2\""
 fi
